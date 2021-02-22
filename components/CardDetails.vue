b6b03e23971a499917a5426162c1deacc0cab15e
Zareen Price <yazatamorph@gmail.com>
2021-02-22 15:54:48 -0500

    Refactored card page and CardDetails component to use Scryfall API directly.

--- components/CardDetails.vue
+++ components/CardDetails.vue
@@ -5,12 +5,14 @@
         <v-col cols="12">
           <CardImage
             :image-source="
-              cardFaces && cardFaces.length && cardFaces[0].image_uris
+              card.card_faces &&
+              card.card_faces.length &&
+              card.card_faces[0].image_uris
                 ? [
-                    cardFaces[0].image_uris.normal,
-                    cardFaces[1].image_uris.normal,
+                    card.card_faces[0].image_uris.normal,
+                    card.card_faces[1].image_uris.normal,
                   ]
-                : [cardImage]
+                : [card.image_uris.normal]
             "
             :alt-text="makeImgAlt()"
           />
@@ -19,7 +21,7 @@
       <v-row>
         <v-col cols="12">
           <v-menu
-            v-if="cardPrintings && cardPrintings.length"
+            v-if="prints && prints.length"
             transition="scroll-y-transition"
           >
             <template v-slot:activator="{ on, attrs }">
@@ -29,7 +31,7 @@
               </v-btn>
             </template>
             <v-list>
-              <div v-for="(print, index) in cardPrintings" :key="print.id">
+              <div v-for="(print, index) in prints" :key="print.id">
                 <v-list-item dense>
                   <v-list-item-content>
                     <nuxt-link
@@ -42,7 +44,7 @@
                     </nuxt-link>
                   </v-list-item-content>
                 </v-list-item>
-                <v-divider v-if="index < cardPrintings.length - 1"></v-divider>
+                <v-divider v-if="index < prints.length - 1"></v-divider>
               </div>
             </v-list>
           </v-menu>
@@ -52,7 +54,7 @@
     <v-col cols="12" sm="9">
       <v-sheet color="transparent" dark class="ml-md-n4 pl-md-4 rounded">
         <v-card-title class="text-h6 mb-n6" style="word-break: normal">{{
-          cardName
+          card.name
         }}</v-card-title>
         <v-list-item>
           <v-list-item-avatar tile>
@@ -65,66 +67,58 @@
             <v-list-item-title>{{ makePrintingInfo() }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <CollectionPanel :card-id="cardId" :set-abbr="setAbbr" :num="num" />
-        <!-- <CardTextPanel /> -->
+        <CollectionPanel
+          :card-id="cardId"
+          :set-abbr="card.set"
+          :num="card.collector_number"
+        />
       </v-sheet>
     </v-col>
-    <!-- <v-col cols="12" md="5" md-offset="6">
-      <v-sheet class="my-md-n4 ml-md-n4 py-md-4 pl-md-4 rounded" outlined>
-        <CollectionPanel :card-id="cardId" :set-abbr="setAbbr" :num="num" />
-      </v-sheet>
-    </v-col> -->
   </v-row>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import _ from 'lodash';
 import CardImage from './CardImage';
-// import CardTextPanel from './CardTextPanel';
 import CollectionPanel from './CollectionPanel';
 
 export default {
   name: 'CardDetails',
   components: {
     CardImage,
-    // CardTextPanel,
     CollectionPanel,
   },
   props: {
     cardId: { type: String, required: true, default: '' },
-    num: { type: String, required: true, default: '' },
-    setAbbr: { type: String, required: true, default: '' },
-  },
-  computed: {
-    ...mapGetters('cards', [
-      'cardArtist',
-      'cardCreaturePower',
-      'cardCreatureTough',
-      'cardFaces',
-      'cardImage',
-      'cardManaCost',
-      'cardName',
-      'cardOracle',
-      'cardPlanesLoyalty',
-      'cardPrintings',
-      'cardTypes',
-      'printingLanguage',
-      'printingRarity',
-      'printingNumber',
-      'setNameAbbreviation',
-      'setName',
-      'setSymbol',
-    ]),
+    card: {
+      type: Object,
+      required: true,
+      default() {
+        return {};
+      },
+    },
+    prints: {
+      type: Array,
+      required: false,
+      default() {
+        return [];
+      },
+    },
+    setSymbol: { type: String, required: true, default: '' },
   },
   methods: {
     makeImgAlt() {
-      return `${this.cardName} (${this.setNameAbbreviation} #${this.printingNumber})`;
+      return `${this.card.name} (${_.toUpper(this.card.set)} #${
+        this.card.collector_number
+      })`;
     },
     makeSetString() {
-      return `${this.setName} (${this.setNameAbbreviation})`;
+      return `${this.card.set_name} (${_.toUpper(this.card.set)})`;
     },
     makePrintingInfo() {
-      return `#${this.printingNumber} | ${this.printingRarity} | ${this.printingLanguage}`;
+      return `#${this.card.collector_number} | ${_.capitalize(
+        this.card.rarity
+      )}`;
     },
   },
 };
--- pages/card/_set/_number/index.vue
+++ pages/card/_set/_number/index.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container>
+    <LoadingOverlay :displaying="searchInProgress" />
     <CardDetails
+      v-if="!searchInProgress"
+      :card="card"
+      :prints="prints"
+      :set-symbol="setSymbol"
       :card-id="cID"
       :set-abbr="$route.params.set.toUpperCase()"
       :num="$route.params.number.toLowerCase()"
@@ -9,33 +14,60 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
 import CardDetails from '~/components/CardDetails';
+import LoadingOverlay from '~/components/LoadingOverlay';
 
 export default {
   layout: 'main',
   components: {
     CardDetails,
+    LoadingOverlay,
   },
   async fetch() {
-    const query = {
-      set: this.$route.params.set,
-      setNum: this.$route.params.number,
-    };
-    await this.queryCard(query);
+    await this.getCardData();
   },
   data() {
     return {
       cID: `${this.$route.params.set.toUpperCase()}${this.$route.params.number.toLowerCase()}`,
+      card: {},
+      prints: [],
+      setSymbol: '',
+      searchInProgress: false,
+      set: this.$route.params.set,
+      setNum: this.$route.params.number,
     };
   },
-  computed: {
-    ...mapState('cards', {
-      card: (state) => state.card,
-    }),
-  },
   methods: {
-    ...mapActions('cards', ['queryCard']),
+    async getCardData() {
+      try {
+        this.searchInProgress = true;
+        const data = await this.$axios.$get(
+          `https://api.scryfall.com/cards/${this.set}/${this.setNum}`
+        );
+
+        this.card = data;
+
+        await Promise.all([
+          this.$axios.$get(this.card.prints_search_uri).then((res) => {
+            this.prints = res.data.filter(
+              (print) =>
+                this.card.set !== print.set ||
+                this.card.collector_number !== print.collector_number
+            );
+          }),
+          this.$axios
+            .$get(`https://api.scryfall.com/sets/${this.card.set}`)
+            .then((res) => {
+              this.setSymbol = res.icon_svg_uri;
+            }),
+        ]);
+
+        this.searchInProgress = false;
+      } catch (err) {
+        this.searchInProgress = false;
+        console.error('Problem getting card data from Scryfall API:', err);
+      }
+    },
   },
   fetchOnServer: false,
 };
