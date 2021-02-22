8d7614302b5849e6d34cd7c698d65e33f0ef564c
Zareen Price <yazatamorph@gmail.com>
2021-02-22 02:01:04 -0500

    Added loading overlay component.

---
+++ components/LoadingOverlay.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-overlay :value="displaying">
+    <v-progress-circular indeterminate size="64"
+      ><v-img max-height="48" max-width="48" src="/hedron.png"></v-img
+    ></v-progress-circular>
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  name: 'LoadingOverlay',
+  props: {
+    displaying: { type: Boolean, required: true, default: false },
+  },
+};
+</script>
